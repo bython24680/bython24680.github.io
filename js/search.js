903483a79bb0f53f60ee372b7ef5c02555416721
Bython <bython24680@gmail.com>
2026-03-15 20:59:55 +0800

    Site updated: 2026-03-15 20:59:54

--- js/search.js
+++ js/search.js
@@ -1,157 +1,86 @@
-// A local search script with the help of
-// [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
-// Copyright (C) 2015
-// Joseph Pan <http://github.com/wzpan>
-// Shuhao Mao <http://github.com/maoshuhao>
-// This library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2.1 of the
-// License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA
-//
-// Modified by:
-// Pieter Robberechts <http://github.com/probberechts>
-
-/*exported searchFunc*/
-var searchFunc = function(path, searchId, contentId) {
-
-  function stripHtml(html) {
-    html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
-    html = html.replace(/<script([\s\S]*?)<\/script>/gi, "");
-    html = html.replace(/<figure([\s\S]*?)<\/figure>/gi, "");
-    html = html.replace(/<\/div>/ig, "\n");
-    html = html.replace(/<\/li>/ig, "\n");
-    html = html.replace(/<li>/ig, "  *  ");
-    html = html.replace(/<\/ul>/ig, "\n");
-    html = html.replace(/<\/p>/ig, "\n");
-    html = html.replace(/<br\s*[\/]?>/gi, "\n");
-    html = html.replace(/<[^>]+>/ig, "");
-    return html;
-  }
-
-  function getAllCombinations(keywords) {
-    var i, j, result = [];
-
-    for (i = 0; i < keywords.length; i++) {
-        for (j = i + 1; j < keywords.length + 1; j++) {
-            result.push(keywords.slice(i, j).join(" "));
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // 取得 search.xml 路徑
+    var searchPath = '/search.xml';
+    if (typeof hexo_config !== 'undefined' && hexo_config.root) {
+        searchPath = hexo_config.root + 'search.xml';
     }
-    return result;
-  }
 
-  $.ajax({
-    url: path,
-    dataType: "xml",
-    success: function(xmlResponse) {
-      // get the contents from search data
-      var datas = $("entry", xmlResponse).map(function() {
-        return {
-          title: $("title", this).text(),
-          content: $("content", this).text(),
-          url: $("link", this).attr("href")
-        };
-      }).get();
+    var input = document.getElementById('local-search-input');
+    var resultContent = document.getElementById('local-search-result');
 
-      var $input = document.getElementById(searchId);
-      if (!$input) { return; }
-      var $resultContent = document.getElementById(contentId);
+    // 如果沒有搜尋框，直接返回
+    if (!input || !resultContent) return;
 
-      $input.addEventListener("input", function(){
-        var resultList = [];
-        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
-          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
-        $resultContent.innerHTML = "";
-        if (this.value.trim().length <= 0) {
-          return;
-        }
-        // perform local searching
-        datas.forEach(function(data) {
-          var matches = 0;
-          if (!data.title || data.title.trim() === "") {
-            data.title = "Untitled";
-          }
-          var dataTitle = data.title.trim().toLowerCase();
-          var dataContent = stripHtml(data.content.trim());
-          var dataUrl = data.url;
-          var indexTitle = -1;
-          var indexContent = -1;
-          var firstOccur = -1;
-          // only match artiles with not empty contents
-          if (dataContent !== "") {
-            keywords.forEach(function(keyword) {
-              indexTitle = dataTitle.indexOf(keyword);
-              indexContent = dataContent.indexOf(keyword);
-
-              if( indexTitle >= 0 || indexContent >= 0 ){
-                matches += 1;
-                if (indexContent < 0) {
-                  indexContent = 0;
+    // 載入 XML
+    fetch(searchPath)
+        .then(response => {
+            if (!response.ok) throw new Error("HTTP error " + response.status);
+            return response.text();
+        })
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            var posts = data.querySelectorAll('entry');
+            
+            input.addEventListener('input', function() {
+                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                var resultItems = [];
+                
+                if (this.value.trim().length <= 0) {
+                    resultContent.innerHTML = '';
+                    return;
                 }
-                if (firstOccur < 0) {
-                  firstOccur = indexContent;
+
+                posts.forEach(function(post) {
+                    var title = post.querySelector('title').textContent;
+                    var content = post.querySelector('content').textContent;
+                    var url = post.querySelector('url').textContent;
+                    
+                    var data_title = title.trim().toLowerCase();
+                    var data_content = content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+                    var isMatch = false;
+
+                    // 簡單的關鍵字匹配
+                    keywords.forEach(function(keyword) {
+                        if (data_title.indexOf(keyword) !== -1 || data_content.indexOf(keyword) !== -1) {
+                            isMatch = true;
+                        }
+                    });
+
+                    if (isMatch) {
+                        resultItems.push({
+                            title: title,
+                            content: content,
+                            url: url
+                        });
+                    }
+                });
+
+                // 渲染結果
+                if (resultItems.length === 0) {
+                    resultContent.innerHTML = '<div class="text-center text-slate-400 py-8">沒有找到相關文章</div>';
+                } else {
+                    var html = '<ul class="space-y-3">';
+                    resultItems.forEach(function(item){
+                        // 確保 URL 正確
+                        var itemUrl = item.url;
+                        if (typeof hexo_config !== 'undefined' && hexo_config.root && !itemUrl.startsWith(hexo_config.root)) {
+                             // 簡單處理，實際情況視 search.xml 生成方式而定
+                        }
+
+                        html += `<li class="search-result-item">
+                            <a href="${itemUrl}">
+                                <div class="search-result-item__title">${item.title}</div>
+                                <p class="search-result-item__content">${item.content.replace(/<[^>]+>/g,"").substring(0, 80)}...</p>
+                            </a>
+                        </li>`;
+                    });
+                    html += '</ul>';
+                    resultContent.innerHTML = html;
                 }
-              }
             });
-          }
-          // show search results
-          if (matches > 0) {
-            var searchResult = {};
-            searchResult.rank = matches;
-            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
-            if (firstOccur >= 0) {
-              // cut out 100 characters
-              var start = firstOccur - 20;
-              var end = firstOccur + 80;
-
-              if(start < 0){
-                start = 0;
-              }
-
-              if(start == 0){
-                end = 100;
-              }
-
-              if(end > dataContent.length){
-                end = dataContent.length;
-              }
-
-              var matchContent = dataContent.substr(start, end);
-
-              // highlight all keywords
-              var regS = new RegExp(keywords.join("|"), "gi");
-              matchContent = matchContent.replace(regS, function(keyword) {
-                return "<em class=\"search-keyword\">"+keyword+"</em>";
-              });
-
-              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
-            }
-            searchResult.str += "</li>";
-            resultList.push(searchResult);
-          }
+        })
+        .catch(e => {
+            console.error('Search fetch error:', e);
+            resultContent.innerHTML = '<div class="text-center text-slate-400 py-8 text-sm">無法載入搜尋索引 (search.xml)<br>請確認已安裝 hexo-generator-searchdb</div>';
         });
-		
-        if (resultList.length) {
-          resultList.sort(function(a, b) {
-              return b.rank - a.rank;
-          });
-          var result ="<ul class=\"search-result-list\">";
-          for (var i = 0; i < resultList.length; i++) {
-            result += resultList[i].str;
-          }
-          result += "</ul>";
-          $resultContent.innerHTML = result;
-        }
-      });
-    }
-  });
-};
+});
